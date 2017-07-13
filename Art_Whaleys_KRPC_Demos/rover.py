5b14ac0df667dac3d707aa7b54b508b827b850c5
Art Whaley <artwhaley@gmail.com>
2017-07-13 02:43:31 -0500

    remove rendezvous file for now.  Updated rover.py

--- Art_Whaleys_KRPC_Demos/rover.py
+++ Art_Whaleys_KRPC_Demos/rover.py
@@ -1,24 +1,51 @@
+######################################################################
+### Rover Controller Library and Example
+######################################################################
+###   Like all of the scripts in my folder here, this file contains
+###   functions you might want to include into your own scripts for  
+###   actual use and a demo in the 'main' function that you can just 
+###   run to see how it works.
+###
+###   This file shows how to make a rover navigate to a waypoint.
+###   The function has a few advanced features - most interestingly
+###   it autosaves at a fixed interval, if the rover is stable and 
+###   moving and seems to be intact.  This is an effort to fix the 
+###   frustration of having a rover crash when you're an hour from 
+###   your last quick save.
+######################################################################
+
+
 import krpc
 import time
 import math
 from collections import namedtuple
 from math import atan2,degrees,cos,sqrt,asin,sin,radians
 
-from pid import PID
+from pid import PID  #imports my PID controller from the PID example file!
 
-latlon =  namedtuple('latlon', 'lat lon')
+latlon =  namedtuple('latlon', 'lat lon')  #create a named tuple
 
-def main():
+##############################################################################
+##  Main function -  this exists to demonstrate the use of the library.  If 
+##  you import this library into your own script, main doesn't get called.
+##############################################################################
+def main():  
     conn = krpc.connect()
 
     #Create a waypoint to drive to
     wp1 = conn.space_center.waypoint_manager.add_waypoint(
-        0.5,-78.12345, conn.space_center.active_vessel.orbit.body,"Waypoint1")
+        0.05,-75.0, conn.space_center.active_vessel.orbit.body,"Waypoint1")
 
     #call the rover autopilot
     rover_go(conn, wp1)
 
-def rover_go(conn, waypoint, speed = 10.0, savetime = 300 ):
+    #remove the waypoint when the function returns
+    wp1.remove()
+
+##############################################################################
+##  And here's the function that's actually interesting! 
+##############################################################################
+def rover_go(conn, waypoint, speed = 15.0, savetime = 300 ):
     '''
     Function to drive a rover to the specified waypoint.  Must be called with
     an active KRPC connection and a valid waypoint.   Attempts to bring rover
@@ -26,62 +53,95 @@ def rover_go(conn, waypoint, speed = 10.0, savetime = 300 ):
     interval.  Defaults to 5 minutes.   This and rover speed can be specified
     as optional arguments.  A savetime of 0 turns this feature off.
     '''
-    v=conn.space_center.active_vessel
+
+    ## grab hold of the krpc functions we'll need to drive the rover
+    v=conn.space_center.active_vessel  
     ground_telem=v.flight(v.orbit.body.reference_frame)
     surf_telem=v.flight(v.surface_reference_frame)
     target=latlon(waypoint.latitude, waypoint.longitude)
+    
+    autosave.lastsave = time.time()
+    partslist = v.parts.all
+    there_yet = False;
 
+    ## Setup the PID controllers for steering and throttle.   The steering 
+    ## setpoint is locked to 0 since we'll be feeding in an error number to
+    ## the update function.   
     steering= PID(.01,.01,.001)
     throttle = PID(.5,.01,.001)
     steering.setpoint(0) 
     throttle.setpoint(speed)
-    autosave.lastsave = time.time()
-    there_yet = False;
 
+
+    #The main loop that drives to the way point
     while not there_yet:
 
-        autosave(conn, savetime)
+        autosave(conn, savetime, partslist)  ##call autosave to see if we should save yet
 
-        #Steering
+    ##  Steering control - handles selecting a bearing, comparing it to 
+    ##  current heading and feeding that error in degrees to the PID to
+    ##  get the control correction required.
         location = latlon(ground_telem.latitude, ground_telem.longitude)
         target_heading = heading_for_latlon(target, location)
         course_correct= course_correction(surf_telem.heading, target_heading)
         steer_correct = steering.update(course_correct)
         v.control.wheel_steering= steer_correct
 
-        #Throttle Management
+        #Throttle control  -  tries to maintain the given speed!
+        v.control.brakes = False
         throttsetting=   throttle.update(ground_telem.speed)
         v.control.wheel_throttle = throttsetting
 
-        #Check if we're there
+        #Check if we're there to end the loop
         if distance(target, location, v.orbit.body) < 50:
             there_yet=True
 
+##############################################################################
+###  Autosave function.   Saves if the vessel appears stable and isn't already
+###  stopped, pitched greater than 30 degrees, or showing a different part
+###  count than before.
+##############################################################################
 
-
-
-def autosave(conn, savetime):
+def autosave(conn, savetime, partslist):
     if savetime == 0:
         return
     if time.time() - autosave.lastsave > savetime:
         v=conn.space_center.active_vessel
         telem=v.flight(v.orbit.body.reference_frame)
 
-        if True: # placeholder for a set of tests to see if it's safe to save
-            v.control.throttle = 0.0
+        if safetosave(conn , partslist): 
+            v.control.throttle = 0.0  ## Stop the rover then save
             v.control.brakes = True
             while telem.speed > 0.01:
                 pass
-            time.sleep(.1)
+            time.sleep(.1)  
             conn.space_center.save('rover_ap')
             v.control.brakes = False
-            autosave.lastsave = time.time()
-     
- 
+        autosave.lastsave = time.time()
 
-################################################################
+# function called by autosave to determine if it's safe to save the file!
+# tries to avoid overwriting a good save with one we take AFTER the rover 
+# has crashed or flipped or run into a space tree.
+def safetosave(conn, partslist):
+    v=conn.space_center.active_vessel
+    ground_telem=v.flight(v.orbit.body.reference_frame)
+    surf_telem=v.flight(v.surface_reference_frame)
+
+    if ground_telem.speed < .1: # We might be stuck! 
+        print("stuck")
+        return False
+    if surf_telem.pitch > 25 or surf_telem.roll >25: # We might have rolled!
+        print("roll")
+        return False
+    if len(partslist) is not len(v.parts.all): # We might have lost something?
+        print("broken")
+        return False
+    return True  # all good!
+
+
+##############################################################################
 ##  Navigation Math Functions
-################################################################
+##############################################################################
 def heading_for_latlon(target, location):
 
     lat1 = math.radians(location.lat)
